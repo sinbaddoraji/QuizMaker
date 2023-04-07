80765de092847ed9236da889ae2b9aadd885b882
Osinachi Nwagboso Samuel <osinachinwagboso@gmail.com>
2023-04-07 20:33:32 +0100

    Add project files.

---
+++ TestMaker/Models/Question.cs
@@ -0,0 +1,10 @@
+﻿namespace TestMaker.Models;
+
+public class Question
+{
+	public Guid QuestionId { get; set; }
+	public string Content { get; set; } = null!;
+	public string CorrectAnswerIndex { get; set; } = null!;
+
+	public string[] Choices { get; set; } = null!;
+}
---
+++ TestMaker/Models/Quiz.cs
@@ -0,0 +1,11 @@
+﻿namespace TestMaker.Models
+{
+	public class Quiz
+	{
+		public string Name {get; set; } = null!;
+
+		public string Description { get; set; } = null!;
+
+		public List<Question>? Questions { get; set; }
+	}
+}
