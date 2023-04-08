dda644d492d8986b55b8041f2d744ab75db9b9a2
Osinachi Nwagboso Samuel <osinachinwagboso@gmail.com>
2023-04-08 18:43:15 +0100

    Fixed Login and Register page

---
+++ TestMaker/Pages/Shared/_LoginUserNavbar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TestMaker.Pages.Shared
+{
+    public class _LoginUserNavbarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
